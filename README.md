8578e82ce6b1bbdb3a044318c6bb8e919990fc50
nack1400 <nack1400@naver.com>
2022-08-04 00:30:19 +0900

    Add React to web

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# 리액트 공부 요약 및 정리
+# 리액트 공부 정리 및 테스트 코드
 ---
-- [웹 사이트에 React 추가](summary\01Add_react.md)
+- 웹 사이트에 React 추가
+[html 코드](test\01index.html) / [js 코드](test\01like_button.js)
---
+++ test/01like_button.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/*
+  3. react 컴포넌트 만들기
+  react 컴포넌트를 정의해주는 코드
+*/
+
+// const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked comment number ' + this.props.commentID;
+    }
+
+    // return e(
+    //   'button',
+    //   { onClick: () => this.setState({ liked: true }) },
+    //   'Like'
+    // );
+
+    /* 
+      선택사항. JSX로 React 해보기
+    */
+    // "Like" <button>을 표시
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>
+    );
+  }
+}
+
+
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    const root = ReactDOM.createRoot(domContainer);
+    root.render(
+      e(LikeButton, { commentID: commentID })
+    );
+  });
