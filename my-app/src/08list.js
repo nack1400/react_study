bf5ebec12b5ab87b83b5817fad95509cfee21e04
nack1400 <nack1400@naver.com>
2022-08-29 14:11:18 +0900

    Fix main_concept

--- my-app/src/08list.js
+++ my-app/src/08list.js
@@ -2,12 +2,13 @@ import React from "react";
 
 function NumberList(props) {
   const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-    <li>{number}</li>
-  );
-  return (
-    <ul>{listItems}</ul>
-  );
+  // 각 리스트에 key를 할당
+  const listItems = numbers.map((number) => (
+    <li key={number.toString()}>
+      {number}
+    </li>
+  ));
+  return <ul>{listItems}</ul>;
 }
 
 export default NumberList;
