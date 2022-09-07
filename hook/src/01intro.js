911c5a56543dfe5bb4c9522c732de77fafc551b9
nack1400 <nack1400@naver.com>
2022-09-07 21:55:15 +0900

    Create hook

---
+++ hook/src/01intro.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function Example() {
+  // "count"라는 새로운 상태 값을 정의합니다.
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default Example
--- hook/src/App.js
+++ hook/src/App.js
@@ -1,9 +1,11 @@
-
+import Example from "./01intro";
 
 function App() {
   return (
     <div>
-      <h1>Hi</h1>
+      <h1>1. Hook 소개</h1>
+      <Example />
+      <hr></hr>
     </div>
   );
 }
