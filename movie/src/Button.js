711ecf2f476984ac89458424eeaa8810d3c9d06d
nack1400 <nack1400@naver.com>
2022-09-23 01:03:11 +0900

    Add css module

--- movie/src/Button.js
+++ movie/src/Button.js
@@ -1,5 +1,8 @@
+import styles from "./Button.module.css"
+
 function Button({text}) {
-  return <button>{text}</button>
+  // css를 모듈화해서 사용
+  return <button className={styles.btn}>{text}</button>
 }
 
 export default Button;
