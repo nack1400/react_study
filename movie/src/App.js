56219efb57298dea57a1497b2200316a678a1145
nack1400 <nack1400@naver.com>
2022-09-24 23:23:24 +0900

    Add useState

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,11 +1,15 @@
 import Button from "./Button"
 import styles from "./App.module.css"
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
       <h1 className={styles.title}>Welcome</h1>
-      <Button text={"button"}/>
+      <Button text={"button"} />
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
