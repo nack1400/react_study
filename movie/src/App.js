dbf3c96fb51f335aa04f1e379856335eddb9740a
nack1400 <nack1400@naver.com>
2022-09-25 22:43:15 +0900

    Add useEffect

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,15 +1,28 @@
 import Button from "./Button"
 import styles from "./App.module.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [key, setKey] = useState("")
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKey(event.target.value);
+  console.log("i run all the time");
+  // 한번만 호출하기 위해
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
       <h1 className={styles.title}>Welcome</h1>
       <Button text={"button"} />
       <button onClick={onClick}>click me</button>
+      <input
+        value={key}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
     </div>
   );
 }
