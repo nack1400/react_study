873923675b509cc0fb87423172f151fe0b1b22b0
nack1400 <nack1400@naver.com>
2022-10-02 21:57:22 +0900

    Add routes, components

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,6 +1,7 @@
 // import Button from "./Button"
 // import styles from "./App.module.css"
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie"
 
 function App() {
   const [loading, setLoading] = useState(true);
