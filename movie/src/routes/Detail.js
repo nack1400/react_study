f89bfc44998bafc87403e95f4506c7f474b6448f
nack1400 <nack1400@naver.com>
2022-10-07 12:42:01 +0900

    Add router paramater

--- movie/src/routes/Detail.js
+++ movie/src/routes/Detail.js
@@ -1,5 +1,20 @@
-function Detail(){
-  return <h1>Detail</h1>;
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/moie_details.json?movie?movie_id=${id}`
+      )
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, [])
+  return <h1>Detail {id}</h1>;
 }
 
 export default Detail;
